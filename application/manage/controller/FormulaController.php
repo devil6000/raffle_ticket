d3771efa46a120095a96e61be90b6ed75373b3a0
devil5060 <devil5060@126.com>
2019-05-22 09:27:31 +0800

    2019-05-22

--- application/manage/controller/FormulaController.php
+++ application/manage/controller/FormulaController.php
@@ -70,6 +70,6 @@ class FormulaController extends LoginBaseController {
         $item = $model->getFormula($id);
         $this->assign('item', $item);
         $this->assign('id', $id);
-        return $this->fetch();
+        return $this->fetch('post');
     }
 }
